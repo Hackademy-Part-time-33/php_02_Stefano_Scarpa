aadb98aa7c27c23468ed2838f6e1318430cd13fa
Stefano Scarpa <scarpastefano@icloud.com>
2024-04-18 19:00:02 +0200

    : traccia 2

--- index.php
+++ index.php
@@ -37,3 +37,21 @@ foreach($users as $user){
     }
 };
 
+
+// traccia 2
+// Dato un array di numeri a scelta, scrivere un programma che calcoli la media solo dei numeri pari contenuti all’interno dell’array.
+
+$numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$sum = 0;
+$totPari = 0;
+
+foreach($numbers as $number){
+    if ($number % 2 == 0) {
+        $sum = $sum + $number;
+        $totPari++;
+    };
+};
+
+$mid = $sum / $totPari;
+echo $mid;
+
